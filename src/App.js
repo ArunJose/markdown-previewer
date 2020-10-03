93aac51eb4e1b806d8bab38f3e222c9902f81f41
ArunJose <12997751+ArunJose@users.noreply.github.com>
2020-10-03 15:01:46 +0530

    Fix formatting of placeholder text

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import marked from "marked";
 export default function App() {
   const [text, setText] = useState(`# Welcome to my React Markdown Previewer!
 
-  ## This is a sub-heading...
-  ### Sub-sub-heading
+## This is a sub-heading...
+### Sub-sub-heading
    
-  Heres some code, \`<div></div>\`, between 2 backticks.
+Heres some code, \`<div></div>\`, between 2 backticks.
     
-  \`\`\`
-  // this is multi-line code:
-  #include <stdio.h>
-  void main(){
-    return 0;
-  }
-  \`\`\`
-    
-  **This is bold text**
-    
-  [Link to Google](https://www.google.com)
+\`\`\`
+// this is multi-line code:
+#include <stdio.h>
+void main(){
+  return 0;
+}
+\`\`\`
   
-  > Block Quotes
+**This is bold text**
+  
+[Link to Google](https://www.google.com)
+  
+> Block Quotes
     
-  - List item 1
-  - List item 2
-  - List item 3
-  - List item 4
+- List item 1
+- List item 2
+- List item 3
+- List item 4
     
-    ![Pikachu icon](https://img.icons8.com/color/2x/pikachu-pokemon.png)
-    `);
+![Pikachu icon](https://img.icons8.com/color/2x/pikachu-pokemon.png)
+`);
   const handleEditor = (e) => {
     setText(e.target.value);
   };
