9fa21aea7179a6b43ef48c0c26596fa6d921980a
ArunJose <12997751+ArunJose@users.noreply.github.com>
2020-10-03 14:53:13 +0530

    Make all required freeCodeCamp tests pass 7/8 tests passed. 8th test is optional
    Basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import marked from "marked";
 
 export default function App() {
+  const [text, setText] = useState(`# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### Sub-sub-heading
+   
+  Heres some code, \`<div></div>\`, between 2 backticks.
+    
+  \`\`\`
+  // this is multi-line code:
+  #include <stdio.h>
+  void main(){
+    return 0;
+  }
+  \`\`\`
+    
+  **This is bold text**
+    
+  [Link to Google](https://www.google.com)
+  
+  > Block Quotes
+    
+  - List item 1
+  - List item 2
+  - List item 3
+  - List item 4
+    
+    ![Pikachu icon](https://img.icons8.com/color/2x/pikachu-pokemon.png)
+    `);
+  const handleEditor = (e) => {
+    setText(e.target.value);
+  };
   return (
     <div className="App">
-      <h1>Markdown Previewer</h1>
+      <div id="container">
+        <textarea id="editor" onChange={handleEditor} value={text}></textarea>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(text) }}
+        ></div>
+      </div>
     </div>
   );
 }
